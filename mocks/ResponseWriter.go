b394ee7cb7574308351f255246263988feffe9e3
namantalaycha <naman.talaycha@myntra.com>
2020-06-07 19:59:27 +0530

    changes commited

---
+++ mocks/ResponseWriter.go
@@ -0,0 +1,22 @@
+
+package mocks
+
+import (
+	"net/http"
+)
+
+type ResponseWriter struct {
+	http.ResponseWriter
+	StatusCode int
+}
+func (m *ResponseWriter) Write(w []byte) (int, error) {
+	return 0, nil
+}
+
+func (m *ResponseWriter) Header() http.Header {
+	return http.Header{}
+}
+
+func (m *ResponseWriter) WriteHeader(statusCode int) {
+	m.StatusCode = statusCode
+}
