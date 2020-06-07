4d6bb198cde1ffdf4909af60e3d95b70d007703d
namantalaycha <naman.talaycha@myntra.com>
2020-06-07 22:57:36 +0530

    changes commit

---
+++ jsonschema/schema_helper.go
@@ -0,0 +1,25 @@
+package jsonschema
+
+import (
+	"fmt"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+func Validate(input , schema string ) bool {
+	schemaLoader := gojsonschema.NewReferenceLoader(schema)
+	documentLoader := gojsonschema.NewReferenceLoader(input)
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		panic(err.Error())
+	}
+	if !result.Valid() {
+		fmt.Printf("The document is not valid. see errors :\n")
+		for _, desc := range result.Errors() {
+			fmt.Printf("- %s\n", desc)
+		}
+		return false
+		//	t.FailNow()
+	}
+     return true
+}
