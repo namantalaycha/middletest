f0829f63a0e4923b9fd4c84e3c94732dc6be4aa7
namantalaycha <naman.talaycha@myntra.com>
2020-06-15 01:47:24 +0530

    mock update

--- mocks/ResponseWriter.go
+++ mocks/ResponseWriter.go
@@ -1,14 +1,16 @@
-
 package mocks
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 type ResponseWriter struct {
 	http.ResponseWriter
 	StatusCode int
+	Written    []byte
 }
+
 func (m *ResponseWriter) Write(w []byte) (int, error) {
 	return 0, nil
 }
@@ -17,7 +19,21 @@ func (m *ResponseWriter) Header() http.Header {
 	return http.Header{}
 }
 
+func (m *ResponseWriter) GetBodyJSON() map[string]interface{} {
+	var v map[string]interface{}
+	json.Unmarshal(m.Written, &v)
+	return v
+}
 
+func (m *ResponseWriter) GetBodyJSONArray() []map[string]interface{} {
+	var v []map[string]interface{}
+	json.Unmarshal(m.Written, &v)
+	return v
+}
+
+func (m *ResponseWriter) GetBodyString() string {
+	return string(m.Written)
+}
 
 func (m *ResponseWriter) WriteHeader(statusCode int) {
 	m.StatusCode = statusCode
---
+++ mocks/request.go
@@ -0,0 +1,12 @@
+package mocks
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+)
+
+func RequestBody(body interface{}) io.ReadCloser {
+	b, _ := json.Marshal(body)
+	return ioutil.NopCloser(bytes.NewReader(b))
+}
