4820a161655556bfa1e7f72282482faa93d72657
namantalaycha <naman.talaycha@myntra.com>
2020-06-19 13:10:06 +0530

    change

--- create_commit.sh
+++ create_commit.sh
@@ -14,8 +14,13 @@ echo "        rm -r temp/text.txt" >> ../functional/.git/hooks/pre-commit
 echo "        exit 1" >> ../functional/.git/hooks/pre-commit
 echo "fi" >> ../functional/.git/hooks/pre-commit
 
-echo "python3 slackbot" >> ../functional/.git/hooks/pre-commit
+echo "python3 slackbot.py" >> ../functional/.git/hooks/pre-commit
+
 sed -e 's/:[^:\/\/]/=/g;s/$//g;s/ *=/=/g' ../functional/configure.yaml >> ../functional/.git/hooks/pre-commit
+echo "enter your app password"
+read var
+echo "thanks....."
+echo SMTPPASS=$var >> ../functional/.git/hooks/pre-commit
 echo "SMTPSERVER=smtp.googlemail.com:587" >> ../functional/.git/hooks/pre-commit
 
 echo "sendEmail -f \$SMTPFROM -t \$SMTPTO -u \$SUBJECT -o message-file=temp/text.txt -s \$SMTPSERVER -xu \$SMTPUSER -xp \$SMTPPASS" >> ../functional/.git/hooks/pre-commit
